9d615ba3ec7c6342918604f087adfdb80ef516f4
emamuzo <john.ofagbe@gmail.com>
2023-03-05 21:39:55 -0800

    updated footer

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -230,8 +230,8 @@ function App() {
 					<a
 						rel="noopener noreferrer"
 						target="_blank"
-						href="https://amigoscode.com/p/full-stack-spring-boot-react">
-						Click here to access Fullstack Spring Boot & React for professionals
+						href="https://emamuzo.com">
+						Emamuzo Technologies
 					</a>
 				</Divider>
 			</Footer>
